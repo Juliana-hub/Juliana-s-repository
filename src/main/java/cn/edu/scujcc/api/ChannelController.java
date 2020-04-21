f8c78074459d1277aed298f9175db04330cd44f9
Juliana <2509869312@qq.com>
2020-04-21 09:14:01 +0800

    wanzheng3

--- src/main/java/cn/edu/scujcc/api/ChannelController.java
+++ src/main/java/cn/edu/scujcc/api/ChannelController.java
@@ -24,7 +24,7 @@ import cn.edu.scujcc.service.ChannelService;
 @RestController   
 @RequestMapping("annel")
 public class ChannelController {
-	public static final Logger logger =LoggerFactory.getLogger(ChannelController.class);abcdefgh
+	public static final Logger logger =LoggerFactory.getLogger(ChannelController.class);abcde
 	@Autowired
 	private ChannelService service;
 	/**
